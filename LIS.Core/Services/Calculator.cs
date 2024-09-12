595faf9d221c9d3ad9a36a98f147997fe9653c4d
Carl Sjöholm <carl.sjoholm@gmail.com>
2024-09-13 08:02:18 +1000

    Fix the issue with longest increasing sequence

--- LIS.Core/Services/Calculator.cs
+++ LIS.Core/Services/Calculator.cs
@@ -4,7 +4,7 @@ namespace LIS.Core.Services
 {
     internal class Calculator : ICalculator
     {
-        public List<int> LongestIncreasingSubsequence(string inputSequence)
+        public int[] LongestIncreasingSubsequence(string inputSequence)
         {
             try
             {
@@ -13,65 +13,55 @@ namespace LIS.Core.Services
                     return [];
                 }
 
-                // Step 1: Parse the input string into an array of integers
-                var cleaned = inputSequence.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                var stringArr = inputSequence.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 
-                if (cleaned is null || cleaned.Length == 0)
+                if (stringArr is null || stringArr.Length == 0)
                 {
                     return [];
                 }
 
-                // this will fail if the string cannot be converted to an int, out of scope
-                var numArray = Array.ConvertAll(cleaned, int.Parse);
-                var numsLength = numArray.Length;
+                var intArr = Array.ConvertAll(stringArr, int.Parse);
 
-                // Step 2: Initialize arrays
-                // array keeps track of the length of the longest increasing subsequence ending at each index.
-                var lengths = new int[numsLength];
-                // array helps in reconstructing the sequence by storing indices of previous elements in the LIS.
-                var predecessors = new int[numsLength];
+                var longestSequence = new List<int>();
+                // initialize with first int in sequence
+                var currentSequence = new List<int> { intArr[0] };
 
-                for (var i = 0; i < numsLength; i++)
+                // loop index starting from second int
+                for (var i = 1; i < intArr.Length; i++)
                 {
-                    lengths[i] = 1;
-                    predecessors[i] = -1;
-                }
-
-                // Step 3: Compute the LIS
-                // Two nested loops compare each element with all previous elements to update the lengths and predecessors.
-                for (var i = 0; i < numsLength; i++)
-                {
-                    for (var j = 0; j < i; j++)
+                    // if the current int is greater than the last int in the current sequence
+                    if (intArr[i] > currentSequence[^1])
                     {
-                        if (numArray[i] > numArray[j] && lengths[i] < lengths[j] + 1)
-                        {
-                            lengths[i] = lengths[j] + 1;
-                            predecessors[i] = j;
-                        }
+                        // add it to the current sequence
+                        currentSequence.Add(intArr[i]);
+                    }
+                    else
+                    {   
+                        CheckCurrentGreaterThanLongest();
+
+                        // reset the current sequence to start with the current element
+                        currentSequence.Clear();
+                        currentSequence.Add(intArr[i]);
                     }
                 }
 
-                // Step 4: Find the index of the maximum length of LIS
-                var maxLength = lengths.Max();
-                var maxIndex = Array.IndexOf(lengths, maxLength);
+                CheckCurrentGreaterThanLongest();
 
-                // Step 5: Reconstruct the LIS
-                // Starting from the index of the maximum length, trace back using predecessors to build the LIS.
-                var lis = new List<int>();
+                return [..longestSequence];
 
-                while (maxIndex != -1)
+                void CheckCurrentGreaterThanLongest()
                 {
-                    lis.Add(numArray[maxIndex]);
-                    maxIndex = predecessors[maxIndex];
+                    // if the current sequence is longer than the longest sequence found so far
+                    if (currentSequence.Count > longestSequence.Count)
+                    {
+                        // update the longest sequence to be the current sequence
+                        longestSequence = new List<int>(currentSequence);
+                    }
                 }
-
-                // reverse the LIS to get it in correct order
-                lis.Reverse();
-                return lis;
             }
             catch
             {
-                // generic catch, normally you would catch specific exceptions then the Exception-class in order to reduce the StackTrace
+                // generic catch all
                 return [];
             }
         }
--- ReadMe.md
+++ ReadMe.md
@@ -1,7 +1,21 @@
 # How to run
+These instructions assume that you already have installed Visual Studio 2022 (any edition) and .net 8 sdk.
+They also assume you have some familiarity in cloning from a git-repository.
+You can choose either suboption a or b depending which you find easier.
 
-1. Open Visual Studio, in the menu bar, click "File" and "Open" then "Project/Solution", navigate to this solution and double click it or double click on the solution file using the file explorer.
-2. In Visual Studio, in the menu bar, click "View" and then "Test Explorer".
-3. Locate the "Test explorer", if you have it docked or as a separate popup.
-4. Click the "fast forward" button/icon (Run all tests in view) or if you know the keyboard shortcut please use that.
-5. The tests should run and give a pass result
+1. Clone the solution using git commands from command prompt or your favorite visual git tool to a folder of your choise, a good idea is to clone it somewhere shallow in the folder structure so it's easy to find.
+2. Open Visual Studio, in the menu bar, click "File" and "Open" then "Project/Solution", navigate to this solution (LIS.sln) and double click it or double click on the solution file (LIS.sln) using the file explorer.
+
+a) 
+    1. In Visual Studio, in the menu bar, click "View" and then "Test Explorer".
+    2. Locate the "Test explorer", if you have it docked or as a separate popup.
+    3. Click the "fast forward" button/icon (Run all tests in view) or if you know the keyboard shortcut please use that.
+       <img src="./test-explorer.png" />
+
+b)
+    1. In Visual Studio, if you do not have the "Solution Explorer" open, go to the menu bar and click "View" and then "Solution Explorer".
+    2. Locate the "Solution Explorer", if you have it docked or as a separate popup.
+    3. Expand the "LIS.Tests" project and right click on the "UnitTests.cs" file, then click "Run tests"
+       <img src="./run-tests.png" />
+
+4. The tests should run and give a pass result
